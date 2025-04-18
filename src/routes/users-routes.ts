78a2d33b847f984a523dd96a922619e5a06507e0
Martin Lowe <martinlowe673@gmail.com>
2025-04-18 15:30:26 +0100

    get users from db

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,10 +1,13 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { usersTable } from '../db/schema.js'
+import { db } from '../db/index.js'
 
 const usersRoute = new Hono()
 
-usersRoute.get('/', (c) => {
-  return c.text('Hello from users route!')
+usersRoute.get('/',async (c) => {
+const users = await db.select().from(usersTable)
+return c.json(users)
 })
 
 export default usersRoute
